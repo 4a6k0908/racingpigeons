787881e5dbe44e3e716512377f12ec8a6b7fee2a
cowbear6598 <cowbear6598@gmail.com>
2023-04-19 14:12:41 +0800

    clean code

--- Assets/Scripts/Core/Database/Login/LoginSystem.cs
+++ Assets/Scripts/Core/Database/Login/LoginSystem.cs
@@ -14,11 +14,6 @@ namespace Core.Database.Login
     {
         private readonly AwsUserModel awsUserModel;
 
-        // Cognito 東京
-        private string         cognitoClientKey = "6bkme5fjo0qksgd6jhmqr1e41g";
-        private string         cognitoPoolKey   = "ap-northeast-1_07PSOmDNc";
-        private RegionEndpoint cognitoRegion    = RegionEndpoint.APNortheast1;
-
         public LoginSystem(AwsUserModel awsUserModel)
         {
             this.awsUserModel = awsUserModel;
