f4d294184bdb966594ff3c0549157e86ba3ce3c8
cowbear6598 <cowbear6598@gmail.com>
2023-04-24 14:04:20 +0800

    complete save system tests and remove user login by account and password

--- Assets/Scripts/Core/Save/SaveSystem.cs
+++ Assets/Scripts/Core/Save/SaveSystem.cs
@@ -1,7 +1,23 @@
-﻿namespace Core.Save
+﻿using System.IO;
+using UnityEngine;
+
+namespace Core.Save
 {
     public class SaveSystem
     {
-        
+        public void Save<T>(T saveData, string saveName) where T : class
+        {
+            string savePath = Path.Combine(Application.persistentDataPath, saveName);
+
+            File.WriteAllText(savePath, JsonUtility.ToJson(saveData));
+        }
+
+        public T Load<T>(string fileName)
+        {
+            var filePath = Path.Combine(Application.persistentDataPath, fileName);
+            var content  = File.ReadAllText(filePath);
+
+            return JsonUtility.FromJson<T>(content);
+        }
     }
 }
---
+++ Assets/Scripts/Tests/Editor/Save/SaveSystemTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using Core.Save;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Editor.Save
+{
+    [TestFixture]
+    public class SaveSystemTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            saveSystem = new SaveSystem();
+        }
+
+        private SaveSystem saveSystem;
+
+        [Test]
+        public void _01_Should_Save_Data_To_Json_In_Local_Storage_Success()
+        {
+            var testData = new TestData(1, 5, "test");
+
+            saveSystem.Save(testData, "Test.dat");
+
+            var filePath = Path.Combine(Application.persistentDataPath, "Test.dat");
+            FileShouldExist(filePath);
+        }
+
+        [Test]
+        public void _02_Should_Load_Data_From_Local_Storage_Success_And_Correct()
+        {
+            var testData = saveSystem.Load<TestData>("Test.dat");
+
+            MoneyShouldBe(1, testData.money);
+            CountShouldBe(5, testData.count);
+            NameShouldBe("test", testData.name);
+        }
+
+        private static void NameShouldBe(string expected, string name)
+        {
+            Assert.AreEqual(expected, name);
+        }
+
+        private static void CountShouldBe(int expected, int count)
+        {
+            Assert.AreEqual(expected, count);
+        }
+
+        private static void MoneyShouldBe(int expected, int money)
+        {
+            Assert.AreEqual(expected, money);
+        }
+
+        private void FileShouldExist(string filePath)
+        {
+            Assert.That(File.Exists(filePath));
+        }
+
+        [Serializable]
+        private class TestData
+        {
+            public int    money;
+            public int    count;
+            public string name;
+
+            public TestData(int money, int count, string name)
+            {
+                this.money = money;
+                this.count = count;
+                this.name  = name;
+            }
+        }
+    }
+}
