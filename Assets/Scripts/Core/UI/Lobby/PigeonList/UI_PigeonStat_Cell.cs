85cd80d000e7fbd094b2656e8d716df8c55586cd
cowbear6598 <cowbear6598@gmail.com>
2023-04-27 16:25:39 +0800

    complete infinite scroll but some variable not confirm

--- Assets/Scripts/Core/UI/Lobby/PigeonList/UI_PigeonStat_Cell.cs
+++ Assets/Scripts/Core/UI/Lobby/PigeonList/UI_PigeonStat_Cell.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Mime;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
 using Core.Pigeon.Models;
 using TMPro;
 using UnityEngine;
@@ -9,10 +11,10 @@ namespace Core.UI.Lobby.PigeonList
 {
     public class UI_PigeonStat_Cell : FancyScrollRectCell<PigeonStat, Context>
     {
-        [SerializeField] private Image           avatarImg; // 鴿子的 Avatar
-        
+        [SerializeField] private Image avatarImg; // 鴿子的 Avatar
+
         [SerializeField] private TextMeshProUGUI nameText; // 鴿子的名字
-        
+
         [SerializeField] private Image    favoriteImg; // 最愛
         [SerializeField] private Sprite[] favoriteSprites;
         [SerializeField] private Image    genderImg; // 性別
@@ -20,36 +22,70 @@ namespace Core.UI.Lobby.PigeonList
         [SerializeField] private Image    statusImg; // 狀態
         [SerializeField] private Sprite[] statusSprites;
 
-        [SerializeField] private Image           iqGemImg; // 智力
-        [SerializeField] private TextMeshProUGUI iqLevelText;
-        [SerializeField] private Image           visionGemImg; // 眼睛
-        [SerializeField] private TextMeshProUGUI visionLevelText;
-        [SerializeField] private Image           speedGemImg; // 體型
-        [SerializeField] private TextMeshProUGUI speedLevelText;
-        [SerializeField] private Image           featherSizeGemImg; // 羽型
-        [SerializeField] private TextMeshProUGUI featherSizeLevelText;
-        [SerializeField] private Image           vitalityGemImg; // 心肺
-        [SerializeField] private TextMeshProUGUI vitalityLevelText;
-        [SerializeField] private Image           muscleGemImg; // 肌力
-        [SerializeField] private TextMeshProUGUI muscleLevelText;
-        [SerializeField] private Image           constitutionGemImg; // 腸胃
-        [SerializeField] private TextMeshProUGUI constitutionLevelText;
-        [SerializeField] private Image           featherQualityGemImg; // 羽質
-        [SerializeField] private TextMeshProUGUI featherQualityLevelText;
-        [SerializeField] private Sprite[]        gemSprites; // 寶石的 Sprites
+        [SerializeField] private AbilityData iqAbility;             // 智力
+        [SerializeField] private AbilityData visionAbility;         // 眼睛
+        [SerializeField] private AbilityData speedAbility;          // 體型
+        [SerializeField] private AbilityData featherSizeAbility;    // 羽型
+        [SerializeField] private AbilityData vitalityAbility;       // 心肺
+        [SerializeField] private AbilityData muscleAbility;         // 肌力
+        [SerializeField] private AbilityData constitutionAbility;   // 腸胃
+        [SerializeField] private AbilityData featherQualityAbility; // 羽質
 
-        [SerializeField] private Image consumeBarImg; // 消耗條
-        [SerializeField] private Image tiredBarImg; // 疲勞條
-        [SerializeField] private Image expBarImg; // 經驗質
+        [SerializeField] private Sprite[] gemSprites; // 寶石的 Sprites
 
-        public override void UpdateContent(PigeonStat itemData)
+        [SerializeField] private Image fatigueBarImg; // 消耗條
+        [SerializeField] private Image tiredBarImg;   // 疲勞條
+        [SerializeField] private Image expBarImg;     // 經驗質
+
+        // 更換鴿子資訊
+        public override void UpdateContent(PigeonStat pigeonStat)
         {
+            nameText.text = $"鴿名：{pigeonStat.pigeon_name}";
+
+            genderImg.sprite = genderSprites[pigeonStat.gender];
+
+            iqAbility.SetCurrentLevel(pigeonStat.iq, pigeonStat.max_iq, gemSprites);
+            visionAbility.SetCurrentLevel(pigeonStat.vision, pigeonStat.max_vision, gemSprites);
+            speedAbility.SetCurrentLevel(pigeonStat.speed, pigeonStat.max_speed, gemSprites);
+            featherSizeAbility.SetCurrentLevel(pigeonStat.feather_size, pigeonStat.max_feather_size, gemSprites);
+            vitalityAbility.SetCurrentLevel(pigeonStat.vitality, pigeonStat.max_vitality, gemSprites);
+            muscleAbility.SetCurrentLevel(pigeonStat.muscle, pigeonStat.max_muscle, gemSprites);
+            constitutionAbility.SetCurrentLevel(pigeonStat.constitution, pigeonStat.max_constitution, gemSprites);
+            featherQualityAbility.SetCurrentLevel(pigeonStat.feather_quality, pigeonStat.max_feather_quality, gemSprites);
             
+            // TODO: 確認上限多少，現在假定 100
+            fatigueBarImg.fillAmount = pigeonStat.fatigue / 100.0f;
+            expBarImg.fillAmount     = pigeonStat.exp     / 100.0f;
+
         }
 
-        public void Button_View()
+        public void Button_View() { }
+
+        [Serializable]
+        private class AbilityData
         {
-            
+            [SerializeField] private Image           gemImg;
+            [SerializeField] private TextMeshProUGUI levelText;
+
+            // 設定寶石圖片以及等級文字資訊
+            public void SetCurrentLevel(int stat, int statMax, IReadOnlyList<Sprite> gemSprites)
+            {
+                gemImg.sprite = statMax switch {
+                    < 200            => gemSprites[0],
+                    >= 200 and < 300 => gemSprites[1],
+                    >= 300 and < 400 => gemSprites[2],
+                    >= 400 and < 500 => gemSprites[3],
+                    _                => gemSprites[4],
+                };
+
+                levelText.text = stat switch {
+                    <= 60  => "C",
+                    <= 148 => "B",
+                    <= 232 => "A",
+                    <= 316 => "S",
+                    _      => "SS",
+                };
+            }
         }
     }
 }
--- Assets/Scripts/Core/UI/Lobby/PigeonList/UI_PigeonStat_Scroller.cs
+++ Assets/Scripts/Core/UI/Lobby/PigeonList/UI_PigeonStat_Scroller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Core.Pigeon.Models;
 using UnityEngine;
 using UnityEngine.UI.Extensions;
@@ -20,6 +21,11 @@ namespace Core.UI.Lobby.PigeonList
             Relayout();
         }
 
+        private void Start()
+        {
+            UpdateData(GetTestPigeons());
+        }
+
         public void UpdateData(IList<PigeonStat> pigeonStats)
         {
             UpdateContents(pigeonStats);
@@ -37,5 +43,202 @@ namespace Core.UI.Lobby.PigeonList
         {
             JumpTo(index, 0.5f);
         }
+
+        // TODO: 測試專用，之後刪掉
+
+        #region Test
+
+        public List<PigeonStat> GetTestPigeons()
+        {
+            return new List<PigeonStat> {
+                new() {
+                    age                    = 0,
+                    age_feature            = 0,
+                    breed_id               = null,
+                    breed_name             = null,
+                    constitution           = 200,
+                    exp                    = 10,
+                    father_pigeon_id       = null,
+                    fatigue                = 20,
+                    feather_quality        = 100,
+                    feather_size           = 200,
+                    feature_a              = 0,
+                    feature_b              = 0,
+                    feature_c              = 0,
+                    free_points            = 0,
+                    gender                 = 1,
+                    grand_father_pigeon_id = null,
+                    grand_mother_pigeon_id = null,
+                    iq                     = 50,
+                    level                  = 0,
+                    max_constitution       = 960,
+                    max_feather_quality    = 300,
+                    max_feather_size       = 400,
+                    max_iq                 = 500,
+                    max_muscle             = 200,
+                    max_speed              = 200,
+                    max_vision             = 300,
+                    max_vitality           = 150,
+                    mother_pigeon_id       = null,
+                    muscle                 = 100,
+                    pigeon_id              = null,
+                    pigeon_name            = "Test",
+                    pigeon_pigeonry        = null,
+                    speed                  = 20,
+                    vision                 = 30,
+                    vitality               = 43
+                },
+                new() {
+                    age                    = 0,
+                    age_feature            = 0,
+                    breed_id               = null,
+                    breed_name             = null,
+                    constitution           = 200,
+                    exp                    = 30,
+                    father_pigeon_id       = null,
+                    fatigue                = 50,
+                    feather_quality        = 150,
+                    feather_size           = 200,
+                    feature_a              = 0,
+                    feature_b              = 0,
+                    feature_c              = 0,
+                    free_points            = 0,
+                    gender                 = 1,
+                    grand_father_pigeon_id = null,
+                    grand_mother_pigeon_id = null,
+                    iq                     = 50,
+                    level                  = 0,
+                    max_constitution       = 960,
+                    max_feather_quality    = 200,
+                    max_feather_size       = 400,
+                    max_iq                 = 300,
+                    max_muscle             = 100,
+                    max_speed              = 100,
+                    max_vision             = 100,
+                    max_vitality           = 150,
+                    mother_pigeon_id       = null,
+                    muscle                 = 100,
+                    pigeon_id              = null,
+                    pigeon_name            = "Test2",
+                    pigeon_pigeonry        = null,
+                    speed                  = 20,
+                    vision                 = 30,
+                    vitality               = 43
+                },
+                new() {
+                    age                    = 0,
+                    age_feature            = 0,
+                    breed_id               = null,
+                    breed_name             = null,
+                    constitution           = 200,
+                    exp                    = 10,
+                    father_pigeon_id       = null,
+                    fatigue                = 20,
+                    feather_quality        = 100,
+                    feather_size           = 200,
+                    feature_a              = 0,
+                    feature_b              = 0,
+                    feature_c              = 0,
+                    free_points            = 0,
+                    gender                 = 1,
+                    grand_father_pigeon_id = null,
+                    grand_mother_pigeon_id = null,
+                    iq                     = 50,
+                    level                  = 0,
+                    max_constitution       = 450,
+                    max_feather_quality    = 250,
+                    max_feather_size       = 350,
+                    max_iq                 = 410,
+                    max_muscle             = 150,
+                    max_speed              = 150,
+                    max_vision             = 175,
+                    max_vitality           = 150,
+                    mother_pigeon_id       = null,
+                    muscle                 = 100,
+                    pigeon_id              = null,
+                    pigeon_name            = "Test3",
+                    pigeon_pigeonry        = null,
+                    speed                  = 20,
+                    vision                 = 30,
+                    vitality               = 43
+                },
+                new() {
+                    age                    = 0,
+                    age_feature            = 0,
+                    breed_id               = null,
+                    breed_name             = null,
+                    constitution           = 200,
+                    exp                    = 10,
+                    father_pigeon_id       = null,
+                    fatigue                = 20,
+                    feather_quality        = 100,
+                    feather_size           = 200,
+                    feature_a              = 0,
+                    feature_b              = 0,
+                    feature_c              = 0,
+                    free_points            = 0,
+                    gender                 = 1,
+                    grand_father_pigeon_id = null,
+                    grand_mother_pigeon_id = null,
+                    iq                     = 50,
+                    level                  = 0,
+                    max_constitution       = 250,
+                    max_feather_quality    = 375,
+                    max_feather_size       = 275,
+                    max_iq                 = 250,
+                    max_muscle             = 200,
+                    max_speed              = 200,
+                    max_vision             = 300,
+                    max_vitality           = 150,
+                    mother_pigeon_id       = null,
+                    muscle                 = 100,
+                    pigeon_id              = null,
+                    pigeon_name            = "Test4",
+                    pigeon_pigeonry        = null,
+                    speed                  = 20,
+                    vision                 = 30,
+                    vitality               = 43
+                },
+                new() {
+                    age                    = 0,
+                    age_feature            = 0,
+                    breed_id               = null,
+                    breed_name             = null,
+                    constitution           = 200,
+                    exp                    = 10,
+                    father_pigeon_id       = null,
+                    fatigue                = 20,
+                    feather_quality        = 100,
+                    feather_size           = 200,
+                    feature_a              = 0,
+                    feature_b              = 0,
+                    feature_c              = 0,
+                    free_points            = 0,
+                    gender                 = 1,
+                    grand_father_pigeon_id = null,
+                    grand_mother_pigeon_id = null,
+                    iq                     = 50,
+                    level                  = 0,
+                    max_constitution       = 960,
+                    max_feather_quality    = 300,
+                    max_feather_size       = 400,
+                    max_iq                 = 500,
+                    max_muscle             = 200,
+                    max_speed              = 200,
+                    max_vision             = 300,
+                    max_vitality           = 150,
+                    mother_pigeon_id       = null,
+                    muscle                 = 100,
+                    pigeon_id              = null,
+                    pigeon_name            = "Test5",
+                    pigeon_pigeonry        = null,
+                    speed                  = 20,
+                    vision                 = 30,
+                    vitality               = 43
+                },
+            };
+        }
+
+        #endregion
     }
 }
