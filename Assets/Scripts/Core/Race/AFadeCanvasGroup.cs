1d7d0c134c067a40900cc2fc1029dbc585830747
Allenline78 <113702141+Allenline78@users.noreply.github.com>
2023-06-02 13:21:49 +0800

    0602 update

---
+++ Assets/Scripts/Core/Race/AFadeCanvasGroup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AFadeCanvasGroup : MonoBehaviour
+{
+    public CanvasGroup canvasGroup1;
+    public CanvasGroup canvasGroup2;
+    public CanvasGroup canvasGroup3;
+
+    private void Update()
+    {
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            StartCoroutine(FadeCanvasGroupCoroutine(canvasGroup1, 0, 1, 0.5f, true));
+            StartCoroutine(FadeCanvasGroupCoroutine(canvasGroup2, 1, 0, 0.5f, false));
+            StartCoroutine(FadeCanvasGroupCoroutine(canvasGroup3, 1, 0, 0.5f, false));
+        }
+    }
+
+    private IEnumerator FadeCanvasGroupCoroutine(CanvasGroup canvasGroup, float startAlpha, float endAlpha, float duration, bool isFinalStateActive)
+    {
+        canvasGroup.gameObject.SetActive(true);
+        float timeElapsed = 0;
+
+        while (timeElapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        canvasGroup.alpha = endAlpha;
+        canvasGroup.gameObject.SetActive(isFinalStateActive);
+    }
+}
---
+++ Assets/Scripts/Core/Race/CloseFadeCanvasGroup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CloseFadeCanvasGroup : MonoBehaviour
+{
+    public CanvasGroup canvasGroup2;
+    public string sceneToLoad; // 要加载的Scene的名称
+    public string sceneToUnload; // 要卸载的Scene的名称
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            StartCoroutine(FadeCanvasGroupCoroutine(canvasGroup2, 1, 0, 0.5f, false));
+        }
+    }
+
+    private IEnumerator FadeCanvasGroupCoroutine(CanvasGroup canvasGroup, float startAlpha, float endAlpha, float duration, bool isFinalStateActive)
+    {
+        canvasGroup.gameObject.SetActive(true);
+        float timeElapsed = 0;
+
+        while (timeElapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        canvasGroup.alpha = endAlpha;
+        canvasGroup.gameObject.SetActive(isFinalStateActive);
+
+        // 当淡出完成后，加载新的Scene
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+
+        // 如果已经设定了要卸载的Scene，那么卸载这个Scene
+        if (!string.IsNullOrEmpty(sceneToUnload))
+        {
+            SceneManager.UnloadSceneAsync(sceneToUnload);
+        }
+    }
+}
